21c9e0da605ece51e366715c6dc0f881e041d805
Abigail-Welch <awelc17@lsu.edu>
2024-08-28 14:48:31 -0500

    assignment 1

---
+++ sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background(0);
+fill(254,67,101);
+noStroke();
+ellipse(width/8, height/16, 500,480);
+fill(252,157,154);
+noStroke();
+ellipse(width/10, height/16, 420,380);
+fill(249,205,173);
+noStroke();
+ellipse(width/12, height/16, 320,280);
+fill(200,200,169);
+noStroke();
+ellipse(width/14, height/16, 220,180);
+fill(131,175,155);
+noStroke();
+ellipse(width/18, height/19, 120,100);
+fill(248,177,149);
+noStroke();
+ellipse(width/1.18, height/1.1, 500,480);
+fill(246,114,128);
+noStroke();
+ellipse(width/1.16, height/1.1, 420,380);
+fill(192,108,132);
+noStroke();
+ellipse(width/1.14, height/1.1, 320,280);
+fill(108,91,123);
+noStroke();
+ellipse(width/1.12, height/1.1, 220,180);
+fill(53,92,125);
+noStroke();
+ellipse(width/1.1, height/1.1, 120,100);
+fill(226,217,194);
+noStroke();
+ellipse(width/2.1, height/2, 100,100);
+}
+
+function draw() {
+
+}
